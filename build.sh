4f97e188db4525496760c10e3c10c8378688a0f0
srz_zumix <zumix.cpp@gmail.com>
2018-10-31 10:35:49 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-mkdir docs
+mkdir -p docs
 
 if $TRAVIS; then
 CI_NAME=travis
 CI_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
 fi
 
-mkdir docs/$CI_NAME
+mkdir -p docs/$CI_NAME
 echo $CI_BUILD_NUMBER > ./docs/$CI_NAME/log.txt
